4eb48db53c2927f0ef509e030d9aeadf496981ab
reszka303 <reszka303@op.pl>
2021-06-07 00:03:57 +0200

    34th commit

---
+++ web-technologies/03_js/01_datatypes_variables_operators/01_data-types.js
@@ -0,0 +1,23 @@
+// Typy proste:
+// undefined
+// Boolean
+// Number
+// String
+// BigInt
+// Symbol
+
+//Typy obiektowe:
+// null
+// Object
+// Function
+
+console.log(typeof 15);
+console.log(typeof 2.8);
+console.log(typeof "abc");
+console.log(typeof 'abc');
+console.log("Karol powiedział: \"programowanie jest super\"" + " - String z cudzysłowem w kodzie");
+console.log('Karol powiedział: \"programowanie jest super\"' + ' - String z apostrofem w kodzie');
+console.log(typeof a);
+console.log(typeof [1, 2, 3]);
+console.log(typeof null);
+ 
