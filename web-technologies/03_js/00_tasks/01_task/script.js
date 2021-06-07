23e24d08081133d1e833c17ba098cd5e020c7cf7
reszka303 <reszka303@op.pl>
2021-06-08 00:18:52 +0200

    41st commit

---
+++ web-technologies/03_js/00_tasks/01_task/script.js
@@ -0,0 +1,8 @@
+const days = 123;
+const hours = days * 24;
+const minutes = hours * 60;
+const seconds = minutes * 60;
+const textToDisplay = "Od początku roku minęło " + days + " dni. " + 
+"To " + hours + " godzin, " + minutes + " minut " + "lub " + seconds + " sekund.";
+alert(textToDisplay);
+console.log(textToDisplay);
