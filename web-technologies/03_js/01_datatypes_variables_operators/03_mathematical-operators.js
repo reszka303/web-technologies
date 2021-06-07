d0c756c152aa7dea90c55456ac7412be580d44c6
reszka303 <reszka303@op.pl>
2021-06-07 02:20:32 +0200

    36th commit

---
+++ web-technologies/03_js/01_datatypes_variables_operators/03_mathematical-operators.js
@@ -0,0 +1,11 @@
+let adding = 2 + 5;
+let subtraction = 10 - 5;
+let dividing = 5 / 2;
+let multiplication = 2 * 5;
+let modulo = 5 % 2;
+
+console.log("Wynik dodawania: " + adding);
+console.log("Wynik odejmowania: " + subtraction);
+console.log("Wynik dzielenia: " + dividing);
+console.log("Wynik mnożenia: " + multiplication);
+console.log("Wynik modulo: " + modulo);
