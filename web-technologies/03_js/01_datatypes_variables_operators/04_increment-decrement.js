7020c0a384e1e1a7bede52a520e949aa6e0bfaf7
reszka303 <reszka303@op.pl>
2021-06-07 02:41:22 +0200

    37th commit

---
+++ web-technologies/03_js/01_datatypes_variables_operators/04_increment-decrement.js
@@ -0,0 +1,10 @@
+let number = 5;
+
+console.log("Inkrementacja postfiksowa - najpierw wyświetla i dodaje: " + number++);
+console.log("Wynik: " + number);
+console.log("Inkrementacja prefiksowa - najpierw dodaje i wyświetla: " + ++number);
+console.log(number);
+console.log("Dekrementacja postfiksowa - najpierw wyświetla i odejmuje: " + number--);
+console.log(number);
+console.log("Dekrementacja prefiksowa - najpierw odejmuje i wyświetla: " + --number);
+console.log(number);
