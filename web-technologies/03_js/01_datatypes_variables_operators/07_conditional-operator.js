5c766763ee1ccb6a7a8a6b4335ac9a002e071d6e
reszka303 <reszka303@op.pl>
2021-06-07 14:39:57 +0200

    40th commit

---
+++ web-technologies/03_js/01_datatypes_variables_operators/07_conditional-operator.js
@@ -0,0 +1,9 @@
+let number1 = 100;
+let number2 = 10;
+
+//operator trójargumentowy
+//Jeżeli number1 jest większy od number2 to mamy true i do biggerNumber przypisujemy number1
+//jeśli number1 jest mniejsze od number2 to mamy false i do biggerNumber przypisujemy number2;
+let biggerNumber = number1 > number2 ? number1 : number2;
+
+console.log("Wynik działania operatora trójargumentowego: " + biggerNumber);
