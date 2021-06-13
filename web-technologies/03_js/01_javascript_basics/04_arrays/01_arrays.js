7716742ec8dbc42ad04b1e7f3355b088905ea535
reszka303 <reszka303@op.pl>
2021-06-13 23:45:21 +0200

    48th commit

--- web-technologies/03_js/01_javascript_basics/04_arrays/01_arrays.js
+++ web-technologies/03_js/01_javascript_basics/04_arrays/01_arrays.js
@@ -44,16 +44,78 @@
 // //Tablice w JS są dynamiczne im bliżej im do ArrayListy niź samej tablicy w Javie
 // //W celu dodania kolejnego elementu możemy sie odwołować po prostu do odpowiedniego indeksu i 
 // //przypisać do niego wartość 
-// let numbers8 = [10,20,30,40,50];
+// let numbers8 = [10, 20, 30, 40, 50];
 // numbers8[5] = 60;
 // console.log(numbers8);
 
-//Jeżeli wstawi się jakąś wartość na bardzie odległy i nie istniejący indeks np 9, to rozmiar tablicy
-//zwiększy się do 10 elementów a ze względu na heterogeniczną naturę tablic w JS elementy o indeksach
-//od 5 do 8 będą miały wartość undefined
-let numbers9 = [10,20,30,40,50];
-numbers9[9] = 60;
-console.log(numbers9);
+// //Jeżeli wstawi się jakąś wartość na bardziej odległy i nie istniejący indeks np 9, to rozmiar tablicy
+// //zwiększy się do 10 elementów a ze względu na heterogeniczną naturę tablic w JS elementy o indeksach
+// //od 5 do 8 będą miały wartość undefined
+// let numbers9 = [10, 20, 30, 40, 50];
+// numbers9[9] = 60;
+// console.log(numbers9);
+
+
+// // Tablice w JS udostępniają wiele przydatnych metod
+// // Metoda push(arg) dodaje element arg na końcu tablicy
+// let numbers10 = [10, 20, 30, 40, 50];
+// numbers10.push(60);
+// console.log(numbers10);
+
+// //Metoda pop() pobiera ostatni element z tablicy i go usuwa
+// //Czyli usunie 50 i wyświetli tablicę bez 50
+// let numbers11 = [10, 20, 30, 40, 50];
+// numbers11.pop();
+// console.log(numbers11);
+
+// //Metoda shift() zwraca pierwszy element z tablicy i go usuwa
+// //Czyli usunie 10 i wyświetli tablicę bez 10
+// let numbers12 = [10, 20, 30, 40, 50];
+// numbers12.shift();
+// console.log(numbers12);
+
+
+// //Metoda unshift(arg) dodaje element arg na początku tablicy o wartości podanej wartości
+// //W tablicy dodane zostanie zero na początku tablicy
+// let numbers13 = [10, 20, 30, 40, 50];
+// numbers13.unshift(0);
+// console.log(numbers13);
+
+// //Do iterowania po tablicy możesz wykorzystać pętle fori pętle lub pętle while, ale można
+// //też skorzystać z pętli for-of, która jest odpowiednikiem pętli foreach w Javie
+
+// let numbers14 = [10, 20, 30, 40, 50];
+// for(const number1 of numbers14) {
+//     console.log(number1);
+// }
+
+// //Pętla for-of umożliwia iterowanie po Stringach
+// let word1 = "kot";
+// for(const letter1 of word1) {
+//     console.log(letter1);
+// }
+
+// //Tablice w JS ma wiele wbudowanych metod dzięki czemu możne je łatwo wywołać.
+// //Metoda reverse() odwraca kolejność elementów w tablicy
+// let numbers15 = [10, 20, 30, 40, 50];
+// numbers15.reverse();
+// console.log(numbers15);
+
+// //metoda sortująca w określonej kolejności
+// let numbers16 = [30, 50, 20, 40, 10];
+// console.log("Liczby PRZED sortowaniem:");
+// console.log(numbers16);
+// numbers16.sort();
+// console.log("Liczby PO sortowaniu:");
+// console.log(numbers16);
+
+//metoda indexOf(val) zwraca indeks pierwszego wystąpienia wartość val w tablicy. Zwraca -1
+//gdy val w tablicy nie istnieje
+let numbers17 = [10, 20, 30, 40, 50];
+let index1 = numbers17.indexOf(20);
+console.log("indexOf(20): " + index1);
+
+
 
 
 
