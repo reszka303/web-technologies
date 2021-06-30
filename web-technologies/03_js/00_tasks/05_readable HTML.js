2553fa74d5cc4e865032022886941a36a7b0c40c
reszka303 <reszka303@op.pl>
2021-06-30 18:26:45 +0200

    53rd commit

---
+++ web-technologies/03_js/00_tasks/05_readable HTML.js
@@ -0,0 +1,18 @@
+const title = "Kurs programowania";
+const h1 = "Kurs programowania w JavaScript";
+const p = "Ucz się na praktycznych przykładach, rozwiązując dziesiątki zadań.";
+const price = 299;
+
+const html = 
+`<html>
+    <head>
+        <title>${title}</title>
+    </head>
+    <body>
+        <h2>${h1}</h2>
+        <p>${p}</p>
+        <p>Cena kursu: ${price}</p>
+    </body>
+</html>`;
+
+console.log(html);
